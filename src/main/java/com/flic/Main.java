3196bdd686c5e88f168fa41a60ba1e1bf1020341
FlicHane <2225801665@qq.com>
2025-03-06 17:15:22 +0800

    release1.2

--- src/main/java/com/flic/Main.java
+++ src/main/java/com/flic/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("feat1,fix1");
-
+        System.out.println("大版本更新1.2");
     }
 }
