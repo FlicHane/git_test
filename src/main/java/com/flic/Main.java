d8e76e448a1088f8f127660d07fd7879119e2b81
FlicHane <2225801665@qq.com>
2025-03-06 17:14:07 +0800

    feat1

--- src/main/java/com/flic/Main.java
+++ src/main/java/com/flic/Main.java
@@ -1,8 +1,12 @@
 package com.flic;
 
+/**
+ * @author Administrator
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("feat1");
+        System.out.println("feat1,fix1");
+
     }
 }
