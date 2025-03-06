c6f0cb56d43309f36fa6b23179549c99e60868f2
FlicHane <2225801665@qq.com>
2025-03-06 17:17:46 +0800

    feat2
    (cherry picked from commit 3f66ea728dceef9abf61d06e0170749ff7aa841f)
    
    取回fix2

--- src/main/java/com/flic/Main.java
+++ src/main/java/com/flic/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("feat1,fix1");
         System.out.println("大版本更新1.2");
+        System.out.println("feat2");
         System.out.println("feat2.1");
         System.out.println("feat3");
         System.out.println("大版本更新2");
